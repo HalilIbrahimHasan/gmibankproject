04141152710486a9192d7c5e44c834b5ac0eacc0
Ibrahim Kalin <hisetkal@gmail.com>
2020-12-03 18:45:11 -0500

    all updates ready for remote feature

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,7 +5,11 @@ public class FirstClass {
   //Hi everyone!!
 
 	//Here is new updates I have
+
 	//Here is my second conflict
+
+	//I will have a conflict a brand new conflict
+
 	//here is the first conflict
 
     
